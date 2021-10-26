dbbaa39ac7b9249ea08f3d4bc6070e0a2a650d64
YongKeeTan <yongkee.t@gmail.com>
2021-10-26 11:08:52 +0800

    seeding iniital sensor data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Sensor.destroy_all
+require 'csv'
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath    = 'db/report.csv'
+
+puts 'begin sensor data seeding'
+CSV.foreach(filepath, csv_options) do |row|
+  Sensor.create(timelog: row['timestamp'],
+                device_id: row['id'],
+                device_type: row['type'],
+                status: row['status'] == 'online')
+end
+puts 'seeding sensor data completed'
